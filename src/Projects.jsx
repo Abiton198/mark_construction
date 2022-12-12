9c4d0087229abaeb0c002e260c6ef9ec72200651
abiton198 <abitonp@gmail.com>
2022-12-12 05:31:39 +0200

    .

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,7 +3,7 @@ import weld from './img/weld.jpg'
 
 export default function Projects() {
   return (
-    <div className='row'>
+    <div className='row m-5'>
         <div className='col m-0.5'>
             <img src={weld} alt='' width='230px'/>
             <p>The Lorraine project 2021</p>
